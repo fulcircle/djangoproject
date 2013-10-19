0e07f7237849173b4c096b3c51ac4a3a85d1df39
Vikram Sunkavalli <vsunkavalli@gmail.com>
2013-10-19 16:51:19 -0400

    Skeleton code for registration and login

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from store import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^register$', views.register, name='register')
+    url(r'^login$', views.login, name='login')
+)
--- store/views.py
+++ store/views.py
@@ -1,1 +1,41 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.control.auth import authenticate
+
+def index(request):
+	return HttpResponse("Hello, world.  You're at the store index")
+
+def register(request):
+	if request.method == 'GET':
+		return render(request, 'store/register.html')	
+
+	elif request.method == 'POST':
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		email = request.POST['email']
+		password = request.POST['password']
+
+		user = User.objects.create_user(email, email, password, first_name=first_name, last_name=last_name)
+		user.save()
+
+		return HttpResponse("Successfully registered, wahoo!")
+
+def login(request):
+	if request.method == 'GET':
+		return render(request, 'store/login.html')
+
+	if request.method == 'POST':
+		email = request.POST['email']
+		password = request.POST['password']
+
+		user = authenticate(username=email, password=password)
+		if user is not None:
+			if user.is_active:
+				return HttpResponse("User is authenticated")
+			else:
+				return HttpResponse("User is not authenticated")
+		else:
+			return HttpResponse("Username and password were incorrect.")
+
